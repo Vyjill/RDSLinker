b65d9a4404b73ea7ad54dfebefdd1991d9711670
Ulysse Moreau <ulysse.moreau@outlook.fr>
2021-08-13 17:54:18 +0200

    Add no warranty

--- RDSLinker/App.xaml.cs
+++ RDSLinker/App.xaml.cs
@@ -15,6 +15,12 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+/* This program is free software. It comes without any warranty, to
+     * the extent permitted by applicable law. You can redistribute it
+     * and/or modify it under the terms of the Do What The Fuck You Want
+     * To Public License, Version 2, as published by Sam Hocevar. See
+     * http://www.wtfpl.net/ for more details. */
+
 namespace RDSLinker
 {
     /// <summary>
